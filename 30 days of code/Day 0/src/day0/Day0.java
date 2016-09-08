0ffb84f9e48ebb92c4c09a1031cd9381695ca78f
Isabel Vico Peinado <isavico86@gmail.com>
2016-09-08 11:32:34 +1000

    Added code

---
+++ 30 days of code/Day 0/src/day0/Day0.java
@@ -0,0 +1,15 @@
+package day0;
+
+import java.util.Scanner;
+
+public class Day0 {
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String userInput = scan.nextLine();
+        System.out.println("Hello, World.");
+        System.out.println(userInput);
+    }
+    
+}
